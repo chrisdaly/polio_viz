fcc1aa8bf70a85a28c597ff80fa907d7063b2729
chrisdaly <chrisdaly1988@gmail.com>
2019-09-24 16:50:55 +0100

    Updated colours

--- static/js/viz.js
+++ static/js/viz.js
@@ -10,22 +10,15 @@ let metricsData;
 let rawData;
 let year;
 const colors = [
-	"#d1d3ca",
-	"#c5c692",
-	"#b8ba57",
-	"#aaab19",
-	"#a3a7b6",
-	"#999d84",
-	"#8f934f",
-	"#858817",
-	"#767da4",
-	"#6f7576",
-	"#676e47",
-	"#606514",
-	"#4a5491",
-	"#464f69",
-	"#414a3f",
-	"#3c4412"
+    "#d6abd9",
+    "#8bbce3",
+    "#3e90d0",
+    "#ff3fab",
+    "#882e94",
+    "#3a2489",
+    "#be006b",
+    "#67045e",
+    "#2b0055"
 ];
 
 const n = Math.floor(Math.sqrt(colors.length));
@@ -35,32 +28,32 @@ const incidentsScale = d3.scaleThreshold([0.0001, 0.001, 0.01, 1], d3.range(n));
 const graticule = d3.geoGraticule();
 
 const projection = d3
-	.geoMercator()
-	.scale(width / 3.5 / Math.PI)
-	.rotate([-11, 0])
-	.translate([width / 2, height / 2]);
+    .geoMercator()
+    .scale(width / 3.5 / Math.PI)
+    .rotate([-11, 0])
+    .translate([width / 2, height / 2]);
 
 const path = d3.geoPath().projection(projection);
 
 const svg = d3
-	.select("#viz")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height)
-	.append("g");
+    .select("#viz")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g");
 
 svg.append("path")
-	.datum(graticule)
-	.attr("class", "graticule")
-	.attr("d", path);
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
 
 svg.append("path")
-	.datum({ type: "Sphere" })
-	.attr("class", "sphere")
-	.attr("d", path)
-	.attr("fill", "#f1f1f1")
-	.attr("stroke", "black")
-	.attr("opacity", 0.3);
+    .datum({ type: "Sphere" })
+    .attr("class", "sphere")
+    .attr("d", path)
+    .attr("fill", "#f1f1f1")
+    .attr("stroke", "black")
+    .attr("opacity", 0.3);
 
 // var chart = d3_xy_chart()
 // 	.width(960)
@@ -69,166 +62,166 @@ svg.append("path")
 Promise.all(files.map(f => d3.json(f))).then(init);
 
 function init(datasets) {
-	countriesData = datasets[0];
-	countriesData.objects.countries.geometries = countriesData.objects.countries.geometries.filter(d => !redundantCountries.includes(d.id));
-	rawData = datasets[1];
-	metricsData = d3
-		.nest()
-		.key(d => d.year)
-		.key(d => d.id)
-		.object(rawData);
+    countriesData = datasets[0];
+    countriesData.objects.countries.geometries = countriesData.objects.countries.geometries.filter(d => !redundantCountries.includes(d.id));
+    rawData = datasets[1];
+    metricsData = d3
+        .nest()
+        .key(d => d.year)
+        .key(d => d.id)
+        .object(rawData);
 
-	console.log("countriesData", countriesData);
-	console.log("rawData", rawData);
-	console.log("metricsData", metricsData);
-	draw();
+    console.log("countriesData", countriesData);
+    console.log("rawData", rawData);
+    console.log("metricsData", metricsData);
+    draw();
 
-	// d3.select("#linechart")
-	// 	.datum(
-	// 		d3
-	// 			.nest()
-	// 			.key(d => d.year)
-	// 			.rollup(v => {
-	// 				return d3.mean(v, d => d.incidents); //, d3.mean(v, d => d.coverage)]
-	// 			})
-	// 			.entries(rawData)
-	// 	)
-	// 	.call(chart);
+    // d3.select("#linechart")
+    // 	.datum(
+    // 		d3
+    // 			.nest()
+    // 			.key(d => d.year)
+    // 			.rollup(v => {
+    // 				return d3.mean(v, d => d.incidents); //, d3.mean(v, d => d.coverage)]
+    // 			})
+    // 			.entries(rawData)
+    // 	)
+    // 	.call(chart);
 }
 
 function draw() {
-	countries = svg
-		.selectAll(".country")
-		.data(topojson.feature(countriesData, countriesData.objects.countries).features)
-		.enter()
-		.append("path")
-		.attr("class", "country")
-		.attr("d", path)
-		.attr("opacity", 1)
-		.attr("fill", "#F0F0F0");
+    countries = svg
+        .selectAll(".country")
+        .data(topojson.feature(countriesData, countriesData.objects.countries).features)
+        .enter()
+        .append("path")
+        .attr("class", "country")
+        .attr("d", path)
+        .attr("opacity", 1)
+        .attr("fill", "#F0F0F0");
 }
 
 function filterData(year, metrics) {
-	let data = metricsData[year];
-	let dataFinal = {};
-	Object.keys(data).map(id => {
-		if (data[id] != null) {
-			dataFinal[id] = {};
-			metrics.forEach(metric => {
-				if (data[id][0][metric] != null) {
-					dataFinal[id][metric] = data[id][0][metric];
-				}
-			});
-		}
-	});
-	return dataFinal;
+    let data = metricsData[year];
+    let dataFinal = {};
+    Object.keys(data).map(id => {
+        if (data[id] != null) {
+            dataFinal[id] = {};
+            metrics.forEach(metric => {
+                if (data[id][0][metric] != null) {
+                    dataFinal[id][metric] = data[id][0][metric];
+                }
+            });
+        }
+    });
+    return dataFinal;
 }
 
 const getColor = countryMetrics => {
-	if (Object.entries(countryMetrics).length === 0) return "#F0F0F0";
-	const { coverage = 0, incidents = 0 } = countryMetrics;
-	console.log();
+    if (Object.entries(countryMetrics).length === 0) return "#F0F0F0";
+    const { coverage = 0, incidents = 0 } = countryMetrics;
+    console.log();
 
-	let coverageRank = coverageScale(coverage);
-	let incidentsRank = incidentsScale(incidents);
-	let rank = coverageRank * n + incidentsRank;
-	// console.log(`coverageRank: ${coverageRank}, incidentsRank: ${incidentsRank}, rank: ${coverageRank * n + incidentsRank}, color: ${colors[rank]}`);
-	return colors[rank];
+    let coverageRank = coverageScale(coverage);
+    let incidentsRank = incidentsScale(incidents);
+    let rank = coverageRank * n + incidentsRank;
+    // console.log(`coverageRank: ${coverageRank}, incidentsRank: ${incidentsRank}, rank: ${coverageRank * n + incidentsRank}, color: ${colors[rank]}`);
+    return colors[rank];
 };
 
 function paint(data) {
-	console.log(data);
-	d3.selectAll(".country")
-		.on("mouseover", d => {
-			if (metricsData[year][d.id] != undefined) {
-				return showTooltip(d, metricsData[year][d.id][0]);
-			}
-		})
-		.on("mouseout", hideTooltip)
-		.on("click", d => getColor(data[d.id]))
-		.transition()
-		.duration(1000)
-		.attr("fill", d => {
-			if (data[d.id] != null) {
-				return getColor(data[d.id]);
-			} else {
-				return "#F0F0F0";
-			}
-		});
+    console.log(data);
+    d3.selectAll(".country")
+        .on("mouseover", d => {
+            if (metricsData[year][d.id] != undefined) {
+                return showTooltip(d, metricsData[year][d.id][0]);
+            }
+        })
+        .on("mouseout", hideTooltip)
+        .on("click", d => getColor(data[d.id]))
+        .transition()
+        .duration(1000)
+        .attr("fill", d => {
+            if (data[d.id] != null) {
+                return getColor(data[d.id]);
+            } else {
+                return "#F0F0F0";
+            }
+        });
 }
 
 document.querySelector('input[id="incidents"]').onchange = function() {
-	controlsUpdated();
+    controlsUpdated();
 };
 
 document.querySelector('input[id="coverage"]').onchange = function() {
-	controlsUpdated();
+    controlsUpdated();
 };
 
 document.getElementById("mySlider").onchange = function() {
-	controlsUpdated();
+    controlsUpdated();
 };
 
 d3.select("#mySlider").on("input", function(d) {
-	controlsUpdated();
-	document.getElementById("year").innerHTML = this.value;
+    controlsUpdated();
+    document.getElementById("year").innerHTML = this.value;
 });
 
 function getMetrics() {
-	let metrics = [];
-	if (document.querySelector('input[id="incidents"]').checked == true) {
-		metrics.push("incidents");
-	} else {
-		metrics.filter(i => i !== "incidents");
-	}
-	if (document.querySelector('input[id="coverage"]').checked == true) {
-		metrics.push("coverage");
-	} else {
-		metrics.filter(i => i !== "coverage");
-	}
-	return metrics;
+    let metrics = [];
+    if (document.querySelector('input[id="incidents"]').checked == true) {
+        metrics.push("incidents");
+    } else {
+        metrics.filter(i => i !== "incidents");
+    }
+    if (document.querySelector('input[id="coverage"]').checked == true) {
+        metrics.push("coverage");
+    } else {
+        metrics.filter(i => i !== "coverage");
+    }
+    return metrics;
 }
 
 function controlsUpdated() {
-	let metrics = getMetrics();
-	year = document.getElementById("mySlider").value;
-	let dataFiltered = filterData(year, metrics);
-	paint(dataFiltered);
+    let metrics = getMetrics();
+    year = document.getElementById("mySlider").value;
+    let dataFiltered = filterData(year, metrics);
+    paint(dataFiltered);
 }
 
 function createToolTip() {
-	tooltip = d3
-		.select("body")
-		.select("#tooltip")
-		.on("mouseover", function(d, i) {
-			tooltip
-				.transition()
-				.duration(0)
-				.style("display", "")
-				.style("opacity", 0.9); // on mouse over cancel circle mouse out transistion
-		})
-		.on("mouseout", function(d, i) {
-			hideTooltip();
-		});
+    tooltip = d3
+        .select("body")
+        .select("#tooltip")
+        .on("mouseover", function(d, i) {
+            tooltip
+                .transition()
+                .duration(0)
+                .style("display", "")
+                .style("opacity", 0.9); // on mouse over cancel circle mouse out transistion
+        })
+        .on("mouseout", function(d, i) {
+            hideTooltip();
+        });
 }
 
 function showTooltip(dTopo, dMetrics) {
-	var coords = path.centroid(dTopo);
-	tooltip = d3
-		.select("body")
-		.select("#tooltip")
-		.style("left", coords[0] + "px")
-		.style("top", coords[1] + "px")
-		.style("opacity", 0)
-		.style("display", "");
+    var coords = path.centroid(dTopo);
+    tooltip = d3
+        .select("body")
+        .select("#tooltip")
+        .style("left", coords[0] + "px")
+        .style("top", coords[1] + "px")
+        .style("opacity", 0)
+        .style("display", "");
 
-	tooltip
-		.transition()
-		.duration(800)
-		.style("opacity", 1);
+    tooltip
+        .transition()
+        .duration(800)
+        .style("opacity", 1);
 
-	tooltip.html(
-		`
+    tooltip.html(
+        `
         <div id="tooltip-Container">
             <table class="tg">
                 <tr>
@@ -244,20 +237,20 @@ function showTooltip(dTopo, dMetrics) {
             </table>
         </div>
         `
-	);
+    );
 }
 
 function hideTooltip() {
-	d3.select("body")
-		.select("#tooltip")
-		.transition()
-		.duration(500)
-		.style("opacity", 0)
-		.transition()
-		.duration(1)
-		.style("display", "None");
-	// .style("left", 0 + "px")
-	// .style("top", 0 + "px");
+    d3.select("body")
+        .select("#tooltip")
+        .transition()
+        .duration(500)
+        .style("opacity", 0)
+        .transition()
+        .duration(1)
+        .style("display", "None");
+    // .style("left", 0 + "px")
+    // .style("top", 0 + "px");
 }
 
 // var formatDate = d3.timeFormat("%Y");
